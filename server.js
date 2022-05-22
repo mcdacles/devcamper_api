ae9ceb5cedd9fca0fb96615228a673f48a0d53f4
mcdacles <markdacles78@gmail.com>
2022-05-22 14:29:19 +0100

    Mongodb connection

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load ENV vars
 dotenv.config({ path: './config/config.env' });
+
+// Conenct to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -24,9 +29,17 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+      .yellow.bold
   )
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit
+  server.close(() => process.exit(1));
+});
