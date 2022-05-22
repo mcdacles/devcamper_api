d59f5e28cf655d3b91bb02d382c21af7e9876461
mcdacles <markdacles78@gmail.com>
2022-05-22 18:48:57 +0100

    Custom error handling

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 // Load ENV vars
@@ -29,6 +30,9 @@ if (process.env.NODE_ENV === 'developement') {
 // Mount routers
 // Connecting URL to bootcamps route
 app.use('/api/v1/bootcamps', bootcamps);
+
+// Errorhandler
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
